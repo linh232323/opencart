6ff14e856c7f228f40798feefab90011fd8269b7
Linh Ho <linh232324@gmail.com>
2015-03-22 13:03:48 +0700

    First comit

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://genuinetour.local/admin/');
+define('HTTP_CATALOG', 'http://genuinetour.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://genuinetour.local/admin/');
+define('HTTPS_CATALOG', 'http://genuinetour.local/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/Data/Web/genuinet/admin/');
+define('DIR_SYSTEM', '/Volumes/Data/Web/genuinet/system/');
+define('DIR_LANGUAGE', '/Volumes/Data/Web/genuinet/admin/language/');
+define('DIR_TEMPLATE', '/Volumes/Data/Web/genuinet/admin/view/template/');
+define('DIR_CONFIG', '/Volumes/Data/Web/genuinet/system/config/');
+define('DIR_IMAGE', '/Volumes/Data/Web/genuinet/image/');
+define('DIR_CACHE', '/Volumes/Data/Web/genuinet/system/cache/');
+define('DIR_DOWNLOAD', '/Volumes/Data/Web/genuinet/system/download/');
+define('DIR_UPLOAD', '/Volumes/Data/Web/genuinet/system/upload/');
+define('DIR_LOGS', '/Volumes/Data/Web/genuinet/system/logs/');
+define('DIR_MODIFICATION', '/Volumes/Data/Web/genuinet/system/modification/');
+define('DIR_CATALOG', '/Volumes/Data/Web/genuinet/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'genuinetours.com.vn');
+define('DB_USERNAME', 'genuinet_sale');
+define('DB_PASSWORD', '1234Zin.');
+define('DB_DATABASE', 'genuinet_opencart');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://genuinetour.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://genuinetour.local/');
+
+// DIR
+define('DIR_APPLICATION', '/Volumes/Data/Web/genuinet/catalog/');
+define('DIR_SYSTEM', '/Volumes/Data/Web/genuinet/system/');
+define('DIR_LANGUAGE', '/Volumes/Data/Web/genuinet/catalog/language/');
+define('DIR_TEMPLATE', '/Volumes/Data/Web/genuinet/catalog/view/theme/');
+define('DIR_CONFIG', '/Volumes/Data/Web/genuinet/system/config/');
+define('DIR_IMAGE', '/Volumes/Data/Web/genuinet/image/');
+define('DIR_CACHE', '/Volumes/Data/Web/genuinet/system/cache/');
+define('DIR_DOWNLOAD', '/Volumes/Data/Web/genuinet/system/download/');
+define('DIR_UPLOAD', '/Volumes/Data/Web/genuinet/system/upload/');
+define('DIR_MODIFICATION', '/Volumes/Data/Web/genuinet/system/modification/');
+define('DIR_LOGS', '/Volumes/Data/Web/genuinet/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'genuinetours.com.vn');
+define('DB_USERNAME', 'genuinet_sale');
+define('DB_PASSWORD', '1234Zin.');
+define('DB_DATABASE', 'genuinet_opencart');
+define('DB_PREFIX', 'oc_');
